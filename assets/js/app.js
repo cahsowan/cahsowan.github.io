f28c52912f9f64d89814dcda94760e3bcadda2f8
Taufik Rahman <cahsowan@gmail.com>
2014-10-13 21:15:15 +0700

    inject cgBusy module

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('cahsowan', ['ngRoute', 'ngSanitize', 'ui.bootstrap'])
+angular.module('cahsowan', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'cgBusy'])
     .config(function($routeProvider, $locationProvider){
         $routeProvider
             .when('/', {
