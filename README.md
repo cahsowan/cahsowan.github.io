91cdbc30ca9eb695285c76dd24a1f100aa6c3040
Taufik Rahman <cahsowan@gmail.com>
2015-01-04 11:28:40 +0700

    removing old files

--- README.md
+++
@@ -1,2 +0,0 @@
-cahsowan.github.io
-==================
--- assets/js/controllers/categories-tree.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('cahsowan')
-    .controller('CategoriesTreeCtrl', function($scope, $http, $rootScope){
-        $scope.myPromise = $http.get($rootScope.apiBaseUrl + '/api/categories').success(function(data){
-            $scope.categories = data.categories;
-        })
-    })
